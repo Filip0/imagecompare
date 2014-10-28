f92f5aec20c2fa134ce7bd9f1f352ad42b041d7f
Andreas Lepperød <andreas_lepp@hotmail.com>
2014-10-28 15:21:10 +0100

    Added a compare page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,20 @@ class StaticPagesController < ApplicationController
 
 	def about
 	end
+
+	def compare
+		offset1 = rand(Image.count)
+		offset2 = rand(Image.count)
+
+		rand1 = Image.offset(offset1).first
+		rand2 = Image.offset(offset2).first
+		while rand1 == rand2 do
+			offset2 = rand(Image.count)
+			rand2 = Image.offset(offset2).first
+		end
+
+		@image1 = Image.find(rand1);
+		@image2 = Image.find(rand2);
+	end
+
 end
