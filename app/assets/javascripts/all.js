f7c9b1ed79b6a68ea8e949b34789b9f79be771fd
Andreas Lepperød <andreas_lepp@hotmail.com>
2014-11-19 12:07:02 +0100

    Fix animation

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,5 +1,6 @@
 $(window).on('resize load', function() {
     $('body').css({"padding-top": $(".navbar").height()+5 + "px"});
+    $('fav-image-button').hide();
 });
 
 $(document).ready(function() {
@@ -8,10 +9,21 @@ $(document).ready(function() {
 
 	$(".fav-image-div").hover(
 	  function() { 
-	  	$(this).children(".fav-image-button").show();
+	  	$(this).children('.fav-image-button').show(300);
 	  },
-	  function() { 
-	  	$(this).children(".fav-image-button").hide(); 
+	  function() {
+	  	$(this).children('.fav-image-button').hide(300);
 	  }
 	);
+
+	$('.fav-image-button').click( function(){
+	  if ( $(this).children('.fav-image-icon').hasClass('glyphicon-star-empty')) {
+		$(this).children('.fav-image-icon').removeClass('glyphicon-star-empty');
+		$(this).children('.fav-image-icon').addClass('glyphicon-star');
+	  } else {
+	  	$(this).children('.fav-image-icon').removeClass('glyphicon-star');
+		$(this).children('.fav-image-icon').addClass('glyphicon-star-empty');
+	  }
+	});
 });
+
